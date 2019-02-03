d7eb4abafb34b4065db0f33f52c49c46f4ad157f
Ondrej Pecta <opecta@gmail.com>
2019-02-03 13:40:28 +0100

    Added option for setting binding texts language
    Signed-off-by: Ondrej Pecta <opecta@gmail.com>

--- addons/binding/org.openhab.binding.jablotron/src/main/java/org/openhab/binding/jablotron/config/JablotronConfig.java
+++ addons/binding/org.openhab.binding.jablotron/src/main/java/org/openhab/binding/jablotron/config/JablotronConfig.java
@@ -17,7 +17,7 @@ package org.openhab.binding.jablotron.config;
 public class JablotronConfig {
     private String login;
     private String password;
-    private String thingUid;
+    private String lang;
     private int refresh;
 
     public String getLogin() {
@@ -36,19 +36,15 @@ public class JablotronConfig {
         this.password = password;
     }
 
-    public String getThingUid() {
-        return thingUid;
-    }
-
-    public void setThingUid(String thingUid) {
-        this.thingUid = thingUid;
-    }
-
     public int getRefresh() {
         return refresh;
     }
 
     public void setRefresh(int refresh) {
         this.refresh = refresh;
+    }
+
+    public String getLang() {
+        return lang;
     }
 }
