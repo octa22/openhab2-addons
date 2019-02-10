7862cc5a134b212e026a65e56a09c5750164e651
Alexander Egger <alexander.egger@gmail.com>
2019-02-10 21:23:06 +0100

    Added file headers required by openhab.

--- addons/binding/org.openhab.binding.jablotron/src/main/java/org/openhab/binding/jablotron/JablotronBindingConstants.java
+++ addons/binding/org.openhab.binding.jablotron/src/main/java/org/openhab/binding/jablotron/JablotronBindingConstants.java
@@ -1,10 +1,14 @@
 /**
- * Copyright (c) 2010-2018 by the respective copyright holders.
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.jablotron;
 
@@ -33,8 +37,7 @@ public class JablotronBindingConstants {
     public static final String CHANNEL_LAST_EVENT = "lastEvent";
     public static final String CHANNEL_LAST_EVENT_CLASS = "lastEventClass";
     public static final String CHANNEL_LAST_EVENT_TIME = "lastEventTime";
-    public static final String CHANNEL_LAST_CHECK_TIME = "lastCheckTime";
-
+   sdf
     // List of all OASIS Channel ids
     public static final String CHANNEL_LAST_EVENT_CODE = "lastEventCode";
     public static final String CHANNEL_STATUS_A = "statusA";
